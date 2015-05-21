3630b14fdb7c3cf62375a09f17337903fa97e942
Steevo13 <Stephanjohan@gmail.com>
2015-05-21 10:34:19 +0200

    Update Homework12.php

--- Homework12.php
+++ Homework12.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h1>Homework 12</h1>
 <?php
-echo "Stephan le Roux  13034007";
-echo "Kevin du Pont  13024087";
-echo "Samuel Swart  13018681"
+
+
+echo "Stephan le Roux  13034007<br>";
+echo "Kevin du Pont  13024087<br>";
+echo "Samuel Swart  13018681";
 ?>
-</body>
-</html>
+
